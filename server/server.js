a8340c9f1fcab12189ecfd3674db9732c46f6ece
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-02-11 22:16:58 +0400

    node

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("Salam")
+var http = require('http');
+
+var server = http.createServer((req, res) => {
+	console.log(res);
+});
+
+server.listen("5555", 'localhost', () => {
+	console.log("5555 port dinlenir");
+});
