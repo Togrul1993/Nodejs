304a1063e06f2631aac3fa3087e9667f1f151319
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-02-13 10:46:45 +0400

    node

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 var http = require('http');
+const fs = require('fs');
+
 
 var server = http.createServer((req, res) => {
-	console.log(res);
+	console.log(req.url, req.method);
+
+	fs.readFile('./html/index.html', (err, data) => {
+		if (err) {
+			console.log(err);
+			res.end()
+		} else {
+			//res.write(data);
+			res.end(data);
+		}
+	});
 });
 
 server.listen("5555", 'localhost', () => {
